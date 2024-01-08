1ee6a912669a974fd4cd71578775788445e24644
ndark <dmitriy.blyndaruk@gmail.com>
2024-01-08 18:46:55 +0200

    add prisma db, test ssr fetch

---
+++ app/snippets/new/page.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+
+interface Props {
+
+}
+
+const CreateSnippet: FC<Props> = ({  }) => {
+  return (
+    <div>Create snip</div>
+  );
+};
+
+export default CreateSnippet
